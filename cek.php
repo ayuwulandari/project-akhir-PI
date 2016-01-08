a0f01fb397babdb2f244dc456fb2042cf655f60b
Ayu Wulandari <aiiuk_wulandari@yahoo.co.id>
2016-01-08 18:25:11 +0700

    Create cek.php

---
+++ cek.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div id="content">
+  <div class="inner">
+    <div id="products">
+      <h2>Cek Status Kiriman</h2>
+      <form>
+        <div>
+          <h4>Masukkan No. Resi :</h4>
+          <input type="text" name="resi" class="lg"/>
+        </div>
+        <div style="margin-top:10px">
+          <input type="submit" name="check" value="Check" class="btn"/>
+        </div>
+      </form>
+      <h2>Cek Tarif</h2>
+      <form>
+        <div>
+          <h4>Dari :</h4>
+          <input type="text" name="dari" class="lg"/>
+        </div>
+        <div>
+          <h4>Menuju :</h4>
+          <input type="text" name="menuju" class="lg"/>
+        </div>
+        <div>
+          <h4>Berat/Kg :</h4>
+          <input type="text" name="berat" class="lg"/>
+        </div>
+        <div style="margin-top:10px">
+          <input type="submit" name="check" value="Check" class="btn"/>
+        </div>
+      </form>
+    </div>
+    <!-- End Inner -->
+  </div>
+  <!-- End Content -->
+</div>
+<!-- End Main -->
+<!-- Begin Footer -->
+</body>
+</html>
