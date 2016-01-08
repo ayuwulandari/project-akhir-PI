36345cece44b9ef0b72ef9fa3bcf92f0e81fec10
Ayu Wulandari <aiiuk_wulandari@yahoo.co.id>
2016-01-08 18:17:04 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,10 @@
+<h2>Kantor Pusat</h2>
+<!-- Begin Row -->
+<div class="row">
+  <p>Jl. Imam Bonjol No. 1</p>
+  <p>Semarang</p>
+  <p>Indonesia</p>
+  <p>Contact Center. (024) 7777 7777</p>
+  <p>Office. (024) 707 7070</p>
+  <p>Fax. (024) 9090909</p>
+</div>
